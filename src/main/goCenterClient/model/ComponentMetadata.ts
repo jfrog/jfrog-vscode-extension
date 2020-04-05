6d29db43837f1c3b4df52ad455e61eda8f494c9e
Yahav Itzhak <yahavi@users.noreply.github.com>
2020-04-05 16:20:00 +0300

    GoCenter client (#32)

---
+++ src/main/goCenterClient/model/ComponentMetadata.ts
@@ -0,0 +1,13 @@
+import { IVulnerabilities } from './Vulnerabilities';
+
+export interface IComponentMetadata {
+    component_id: string;
+    description: string;
+    licenses: string[];
+    contributors: number;
+    stars: number;
+    gocenter_readme_url: string;
+    gocenter_metrics_url: string;
+    vulnerabilities: IVulnerabilities;
+    error: string;
+}
---
+++ src/main/goCenterClient/model/Vulnerabilities.ts
@@ -0,0 +1,6 @@
+import { ISeverityCount } from './SeverityCount';
+
+export interface IVulnerabilities {
+    severity: ISeverityCount;
+    gocenter_security_url: string;
+}
