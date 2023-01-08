5389e8dc2ba0595d0a48e8f70c066b2a880d02e6
attiasas <assafattias93@gmail.com>
2023-01-08 10:10:29 +0200

    add lbl, tooltip tests

--- src/test/tests/fileTreeNode.test.ts
+++ src/test/tests/fileTreeNode.test.ts
@@ -4,6 +4,7 @@ import { FileTreeNode } from '../../main/treeDataProviders/issuesTree/fileTreeNo
 import { IssuesRootTreeNode } from '../../main/treeDataProviders/issuesTree/issuesRootTreeNode';
 import { Severity, SeverityUtils } from '../../main/types/severity';
 import { createTestNode, FileNodeTestCase, FileNodeTestData } from './utils/treeNodeUtils.test';
+import { Utils } from '../../main/treeDataProviders/utils/utils';
 
 /**
  * Test functionality of @class FileTreeNode.
@@ -12,20 +13,20 @@ describe('File Node Tests', () => {
     let testCases: any[] = [
         {
             test: 'No issues',
-            data: { path: 'folder/path', issues: [] },
+            data: { path: '/root/folder/path', issues: [] },
             expectedSeverity: Severity.Unknown,
             expectedDescription: ''
         } as FileNodeTestCase,
         {
             test: 'One issue',
-            data: { path: 'folder/path', issues: [Severity.Medium] } as FileNodeTestData,
+            data: { path: '/root/folder/path', issues: [Severity.Medium] } as FileNodeTestData,
             expectedSeverity: Severity.Medium,
             expectedDescription: ''
         } as FileNodeTestCase,
         {
             test: 'Multiple issues',
             data: {
-                path: 'folder/path',
+                path: '/root/folder/path',
                 issues: [Severity.Low, Severity.Low, Severity.NotApplicableCritical, Severity.NotApplicableHigh, Severity.High]
             } as FileNodeTestData,
             expectedSeverity: Severity.High,
@@ -48,24 +49,30 @@ describe('File Node Tests', () => {
     });
 
     testCases.forEach(testCase => {
-        it('label test - ' + testCase.test, () => {
-            //
+        it('label/name test - ' + testCase.test, () => {
+            let testNode: FileTreeNode = createTestNode(testCase);
+            assert.equal(testNode.name, Utils.getLastSegment(testCase.data.path));
+            assert.equal(testNode.label, Utils.getLastSegment(testCase.data.path));
         });
     });
 
     testCases.forEach(testCase => {
         it('Description test - ' + testCase.test, () => {
             let testNode: FileTreeNode = createTestNode(testCase);
-            // Before parent
+            // No parent
             assert.equal(testNode.description,testNode.fullPath);
-            // Parent not in path
-            testNode.parent = new IssuesRootTreeNode({} as vscode.WorkspaceFolder);
+            // Local path not in parent path
+            testNode.parent = new IssuesRootTreeNode({uri: { fsPath: "nowhere"} as vscode.Uri } as vscode.WorkspaceFolder);
             testNode.apply();
             assert.equal(testNode.description,testNode.fullPath);
-            // Parent in path
-            testNode.parent = new IssuesRootTreeNode({ uri: { fsPath: "folder" } as vscode.Uri } as vscode.WorkspaceFolder);
+            // file in root
+            testNode.parent = new IssuesRootTreeNode({ uri: { fsPath: "/root/folder"} as vscode.Uri } as vscode.WorkspaceFolder);
             testNode.apply();
-            assert.equal(testNode.description,"./path");
+            assert.equal(testNode.description,undefined);
+            // Parent in path and file not in root
+            testNode.parent = new IssuesRootTreeNode({ uri: { fsPath: "/root"} as vscode.Uri } as vscode.WorkspaceFolder);
+            testNode.apply();
+            assert.equal(testNode.description,"./folder/path");
         });
     });
 
@@ -77,9 +84,16 @@ describe('File Node Tests', () => {
 
     testCases.forEach(testCase => {
         it('Tooltip test - ' + testCase.test, () => {
-            // issues count
-            // timestamp
-            // top severity
+            let testNode: FileTreeNode = createTestNode(testCase);
+            assert.equal(testNode.fullPath,testCase.data.path);
+            assert.include(testNode.tooltip, "Full path: " + testNode.fullPath);
+            assert.equal(testNode.issues.length,testCase.data.issues.length);
+            assert.include(testNode.tooltip, "Issues count: " + testNode.issues.length);
+            // timestamp - not set
+            assert.notInclude(testNode.tooltip, "Last scan completed at");
+            testNode.timeStamp = Date.now();
+            testNode.apply();
+            assert.include(testNode.tooltip, "Last scan completed at");
         });
     });
 
@@ -91,8 +105,8 @@ describe('File Node Tests', () => {
         },
         {
             test: 'Failed node with reason',
-            data: FileTreeNode.createFailedScanNode('path', 'reason'),
-            expectedName: 'path - reason'
+            data: FileTreeNode.createFailedScanNode('path', '[reason]'),
+            expectedName: 'path - [reason]'
         }
     ].forEach(testCase => {
         it(testCase.test, () => {
