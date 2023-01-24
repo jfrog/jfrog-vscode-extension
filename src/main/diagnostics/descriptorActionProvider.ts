cd84eeb4789ed0d48b5ac29342978144ea2823cb
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2023-01-24 10:04:48 +0200

    Add jump from dependency in tree to location in editor (#246)

--- src/main/diagnostics/descriptorActionProvider.ts
+++ src/main/diagnostics/descriptorActionProvider.ts
@@ -97,7 +97,7 @@ export class DescriptorActionProvider extends AbstractFileActionProvider impleme
             // Calculate the direct dependency information of each issue in the descriptor from the impact tree
             fileIssues.dependenciesWithIssue.forEach(dependencyWithIssue => {
                 dependencyWithIssue.issues.forEach(issue => {
-                    issue.impactedTree?.children
+                    issue.impactedTree.children
                         ?.map(impact => impact.name)
                         .forEach(directDependencyId =>
                             this.handleIssueInDirectDependency(
