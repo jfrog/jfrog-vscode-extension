6fdbfe978c6e46e6a61fca09602668519e486366
attiasas <assafattias93@gmail.com>
2023-01-24 15:24:40 +0200

    review changes

--- src/main/treeDataProviders/issuesTree/descriptorTree/dependencyIssuesTreeNode.ts
+++ src/main/treeDataProviders/issuesTree/descriptorTree/dependencyIssuesTreeNode.ts
@@ -6,6 +6,7 @@ import { DescriptorTreeNode } from './descriptorTreeNode';
 import { IComponent } from 'jfrog-client-js';
 import { CveTreeNode } from './cveTreeNode';
 import { LicenseIssueTreeNode } from './licenseIssueTreeNode';
+import { ContextKeys } from '../../../constants/contextKeys';
 
 export class DependencyIssuesTreeNode extends vscode.TreeItem {
     // Infer from data
@@ -25,6 +26,7 @@ export class DependencyIssuesTreeNode extends vscode.TreeItem {
         this._version = component.package_version;
         this._type = toPackageType(component.package_type);
         this.description = this._version + (_indirect ? ' (indirect)' : '');
+        this.contextValue += ContextKeys.COPY_TO_CLIPBOARD_ENABLED;
     }
 
     /**
