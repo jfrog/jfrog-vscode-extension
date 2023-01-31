13717aafcc1f738595201d890c8dcae0a3c4b46a
attiasas <assafattias93@gmail.com>
2023-01-31 12:38:34 +0200

    remove check

--- src/main/treeDataProviders/issuesTree/codeFileTree/codeIssueTreeNode.ts
+++ src/main/treeDataProviders/issuesTree/codeFileTree/codeIssueTreeNode.ts
@@ -10,9 +10,9 @@ export class CodeIssueTreeNode extends IssueTreeNode {
     constructor(issueId: string, private _parent: CodeFileTreeNode, region: vscode.Range, severity?: Severity, label?: string) {
         super(issueId, severity ?? Severity.Unknown, label ?? issueId, vscode.TreeItemCollapsibleState.None);
         this._regionWithIssue = new vscode.Range(
-            // Analyzer locations minimum is 1, vscode positions starts at 0 
-            new vscode.Position(region.start.line - 1 > 0 ? region.start.line - 1 : 0, region.start.character - 1 > 0 ? region.start.character - 1 : 0),
-            new vscode.Position(region.end.line - 1 > 0 ? region.end.line - 1 : 0, region.end.character - 1 > 0 ? region.end.character - 1 : 0)
+            // Analyzer locations minimum is 1, vscode positions starts at 0
+            new vscode.Position(region.start.line - 1, region.start.character - 1),
+            new vscode.Position(region.end.line - 1, region.end.character - 1)
         );
         this.description = 'line = ' + region.start.line + ', column = ' + region.start.character;
     }
