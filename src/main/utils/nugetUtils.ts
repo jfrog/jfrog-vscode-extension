8b4bf4b25c07dedc1969e5e4e7aff8807f20ca05
attiasas <assafattias93@gmail.com>
2023-02-08 18:01:40 +0200

    fix nuget build graph and add not installed warn

--- src/main/utils/nugetUtils.ts
+++ src/main/utils/nugetUtils.ts
@@ -5,28 +5,33 @@ import { LogManager } from '../log/logManager';
 import { NugetTreeNode } from '../treeDataProviders/dependenciesTree/dependenciesRoot/nugetTree';
 import { DependenciesTreeNode } from '../treeDataProviders/dependenciesTree/dependenciesTreeNode';
 import { TreesManager } from '../treeDataProviders/treesManager';
+import { Utils } from '../treeDataProviders/utils/utils';
 import { ProjectDetails } from '../types/projectDetails';
 
 export class NugetUtils {
+    
+    public static readonly SOLUTION_SUFFIX: string = '.sln';
+    public static readonly PROJECT_SUFFIX: string = '.csproj';
+
     /**
-     * @param solutions        - Paths to *.sln files
+     * @param solutionsAndProjects        - Paths to *.sln files
      * @param componentsToScan - Set of nuget components to populate during the tree building. We'll use this set later on, while scanning the packages with Xray.
      * @param treesManager     - Scan trees manager
      * @param parent           - The base tree node
      * @param quickScan        - True to allow using the scan cache
      */
     public static async createDependenciesTrees(
-        solutions: vscode.Uri[] | undefined,
-        projectsToScan: ProjectDetails[],
+        solutionsAndProjects: vscode.Uri[] | undefined,
+        componentsToScan: ProjectDetails[],
         treesManager: TreesManager,
         parent: DependenciesTreeNode,
         checkCanceled: () => void
     ): Promise<void> {
+        let solutions: vscode.Uri[] | undefined = this.filterSolutions(solutionsAndProjects);
         if (!solutions) {
             treesManager.logManager.logMessage('No *.sln files found in workspaces.', 'DEBUG');
             return;
         }
-
         treesManager.logManager.logMessage('Solution files to scan: [' + solutions.toString() + ']', 'DEBUG');
         for (let solution of solutions) {
             checkCanceled();
@@ -34,14 +39,51 @@ export class NugetUtils {
             if (!tree) {
                 continue;
             }
-            let solutionDir: string = path.dirname(solution.fsPath);
+            let projectsInSolutions: vscode.Uri[] | undefined = this.filterProjects(solutionsAndProjects,solution);
+            
+            let root: NugetTreeNode = this.createSolutionNode(parent, solution, projectsInSolutions, tree, treesManager.logManager);
             for (let project of tree.projects) {
                 checkCanceled();
-                let root: NugetTreeNode = new NugetTreeNode(solutionDir, parent);
-                root.refreshDependencies(project);
-                projectsToScan.push(root.projectDetails);
+                let projectUri: vscode.Uri | undefined = this.getProjectUri(project.name, projectsInSolutions);
+                if (projectUri) {
+                    let projectDir: string = path.dirname(projectUri.fsPath);
+                    let projectNode: NugetTreeNode = new NugetTreeNode(projectDir, root);
+                    projectNode.refreshDependencies(project);
+                }
             }
         }
+    }
+
+    private static createSolutionNode(parent: DependenciesTreeNode, solution: vscode.Uri, projectsInSolutions: vscode.Uri[] | undefined, tree: any, logManager: LogManager): NugetTreeNode {
+        let solutionDir: string = path.dirname(solution.fsPath);
+        let failed: boolean = false;
+        if (projectsInSolutions && projectsInSolutions.length !== tree.projects.length) {
+            logManager.logMessageAndToastErr(
+                `Failed to scan nuget project. Hint: Please make sure the commands 'dotnet restore' or 'nuget restore' run successfully in '${solutionDir}'`,
+                'ERR'
+            );
+            failed = true;
+        }
+        let solutionNode: NugetTreeNode = new NugetTreeNode(solutionDir, parent);
+        solutionNode.setName(Utils.getLastSegment(solution.fsPath) + (failed ? ' [Not installed]' : ''));
+        return solutionNode;
+    }
+
+    private static getProjectUri(projectName: string, solutionsAndProjects: vscode.Uri[] | undefined): vscode.Uri | undefined {
+        return solutionsAndProjects?.find(optional => optional.fsPath.includes(projectName))
+    }
+
+    public static filterProjects(solutionsAndProjects: vscode.Uri[] | undefined, inSolution?: vscode.Uri): vscode.Uri[] | undefined {
+        let projects: vscode.Uri[] | undefined = solutionsAndProjects?.filter(optional => optional.fsPath.endsWith(NugetUtils.PROJECT_SUFFIX));
+        if (!inSolution || !projects) {
+            return projects;
+        }
+        let solutionDir: string = path.dirname(inSolution.fsPath);
+        return projects.filter(optional => optional.fsPath.includes(solutionDir));
+    }
+
+    public static filterSolutions(solutionsAndProjects: vscode.Uri[] | undefined): vscode.Uri[] | undefined {
+        return solutionsAndProjects?.filter(optional => optional.fsPath.endsWith(NugetUtils.SOLUTION_SUFFIX));
     }
 
     /**
@@ -74,4 +116,49 @@ export class NugetUtils {
         }
         return nugetList;
     }
+
+    // /**
+    //  * Get package.json file and return the position of 'dependencies' section.
+    //  * @param document - package.json file
+    //  */
+    // public static getDependenciesPos(document: vscode.TextDocument): vscode.Position[] {
+    //     let res: vscode.Position[] = [];
+    //     let packageJsonContent: string = document.getText();
+    //     let dependenciesMatch: RegExpMatchArray | null = packageJsonContent.match('"((devD)|(d))ependencies"s*:s*');
+    //     if (!dependenciesMatch) {
+    //         return res;
+    //     }
+    //     res.push(document.positionAt(<number>dependenciesMatch.index));
+    //     res.push(new vscode.Position(res[0].line, res[0].character + dependenciesMatch[0].length));
+    //     return res;
+    // }
+
+    // // public static getDependenciesPos(document: vscode.TextDocument): vscode.Position[] {
+
+    // // }
+
+    // /**
+    //  * Get package.json file and dependencies tree node. return the position of the dependency in the package.json file.
+    //  * @param document             - package.json file
+    //  * @param dependenciesTreeNode - dependencies tree node
+    //  */
+    // public static getDependencyPosition(document: vscode.TextDocument, artifactId: string, focusType: FocusType): vscode.Range[] {
+    //     let res: vscode.Range[] = [];
+    //     let packageJsonContent: string = document.getText();
+    //     let dependencyMatch: RegExpMatchArray | null = packageJsonContent.match('("' + artifactId + '"\\s*:\\s*).*"');
+    //     if (!dependencyMatch) {
+    //         return res;
+    //     }
+    //     let startPos: vscode.Position;
+    //     switch (focusType) {
+    //         case FocusType.Dependency:
+    //             startPos = document.positionAt(<number>dependencyMatch.index);
+    //             break;
+    //         case FocusType.DependencyVersion:
+    //             startPos = document.positionAt(<number>dependencyMatch.index + dependencyMatch[1].length);
+    //             break;
+    //     }
+    //     res.push(new vscode.Range(startPos, new vscode.Position(startPos.line, startPos.character + dependencyMatch[0].length)));
+    //     return res;
+    // }
 }
