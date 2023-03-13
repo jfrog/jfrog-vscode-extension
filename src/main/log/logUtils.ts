27619ff53e510df72ad9eb0a2682fd4e55a65820
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2023-03-13 17:42:55 +0200

    Clear oldest logs first when log directory is full (#299)

--- src/main/log/logUtils.ts
+++ src/main/log/logUtils.ts
@@ -11,14 +11,13 @@ export class LogUtils {
         let logFolder: string = ScanUtils.getLogsPath();
         let logFiles: string[] = fs.readdirSync(logFolder).map(fileName => path.join(logFolder, fileName));
         let toRemoveCount: number = logFiles.length + 1 - this.KEEP_LOGS_COUNT;
-        let removed: number = 0;
 
         if (toRemoveCount > 0) {
             let logInfo: [string, number][] = logFiles.map(logPath => [logPath, fs.statSync(logPath).birthtime.getTime()]);
             logInfo.sort(([, lTime], [, rTime]) => lTime - rTime);
-            while (removed < toRemoveCount) {
-                fs.rmSync(logFiles[removed]);
-                removed++;
+            for (let i: number = 0; i < toRemoveCount; i++) {
+                let [logPath] = logInfo[i];
+                fs.rmSync(logPath);
             }
         }
     }
