de1dd7f18194cdc8f9df1e08c347b0cba6407a5f
attiasas <assafattias93@gmail.com>
2023-04-13 18:08:48 +0300

    try fix request test on windows

--- src/test/tests/applicabilityScan.test.ts
+++ src/test/tests/applicabilityScan.test.ts
@@ -50,7 +50,15 @@ describe.only('Contextual Analysis Scan Tests', () => {
     ].forEach(test => {
         it('Check generated Yaml request - ' + test.name, () => {
             let request: ApplicabilityScanArgs = getApplicabilityScanRequest(test.roots, test.cves, test.skip);
-            assert.deepEqual(getDummyRunner().requestsToYaml(request), fs.readFileSync(test.file).toString());
+            assert.deepEqual(
+                getDummyRunner()
+                    .requestsToYaml(request)
+                    .trim(),
+                fs
+                    .readFileSync(test.file)
+                    .toString()
+                    .trim()
+            );
         });
     });
 
