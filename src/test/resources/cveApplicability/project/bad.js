2be420404bf535816e6b826188167bd008e5e36d
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2023-04-18 13:42:58 +0300

    Add applicability scan tests (#323)

--- src/test/resources/cveApplicability/project/bad.js
+++
@@ -1,5 +0,0 @@
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
-var some_dom_id = readline();
-$(some_dom_id).load("whatever.html");
