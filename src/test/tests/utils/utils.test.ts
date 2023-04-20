c21a0a44617b19e3676b9e0d070d1e4e6d7402fc
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2023-04-20 09:37:30 +0300

    Add option to set timeout in settings (#330)

--- src/test/tests/utils/utils.test.ts
+++ src/test/tests/utils/utils.test.ts
@@ -5,6 +5,10 @@ import { ConnectionManager } from '../../../main/connect/connectionManager';
 import { ScanCacheManager } from '../../../main/cache/scanCacheManager';
 import { DependenciesTreeNode } from '../../../main/treeDataProviders/dependenciesTree/dependenciesTreeNode';
 import { TestMemento } from './testMemento.test';
+import { LogManager } from '../../../main/log/logManager';
+import { ContextKeys, SessionStatus } from '../../../main/constants/contextKeys';
+import { ConnectionUtils } from '../../../main/connect/connectionUtils';
+import { JfrogClient } from 'jfrog-client-js';
 
 export function isWindows(): boolean {
     return os.platform() === 'win32';
@@ -37,8 +41,42 @@ export function createScanCacheManager(): ScanCacheManager {
     } as vscode.ExtensionContext);
 }
 
-export function createConnectionManager(): ConnectionManager {
-    return {} as ConnectionManager;
+export async function createTestConnectionManager(logManager: LogManager, timeout?: number, retry?: number): Promise<ConnectionManager> {
+    return await new ConnectionManagerWrapper(logManager, timeout, retry).activate({
+        globalState: {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            get(key: string) {
+                if (key == ContextKeys.SESSION_STATUS) {
+                    return SessionStatus.SignedOut;
+                }
+                return;
+            },
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            update(key: string, value: any) {
+                return;
+            }
+        } as vscode.Memento
+    } as vscode.ExtensionContext);
+}
+
+export class ConnectionManagerWrapper extends ConnectionManager {
+    constructor(logManager: LogManager, private _timeout?: number, private _retry?: number) {
+        super(logManager);
+    }
+
+    /** @override */
+    public createJfrogClient(): JfrogClient {
+        return ConnectionUtils.createJfrogClient(
+            this.url,
+            this.rtUrl,
+            this.xrayUrl,
+            this.username,
+            this.password,
+            this.accessToken,
+            this._retry,
+            this._timeout
+        );
+    }
 }
 
 export function getCliHomeDir(): string {
