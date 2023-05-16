b099bc3a73c92352f6f3516ad4dc915623787bae
Or Geva <orgab@jfrog.com>
2023-05-16 18:14:42 +0300

    Add comments

--- src/main/webview/eventManager.ts
+++ src/main/webview/eventManager.ts
@@ -2,12 +2,18 @@ import { IdeEvent, IdeEventType, SetEmitterEvent, ShowPageEvent, WebviewEvent, W
 import * as vscode from 'vscode';
 import { ScanUtils } from '../utils/scanUtils';
 
+/**
+ * Provide APIs for communication between JFrog extension and the webview.
+ */
 export class WebviewEventManager {
     constructor(webview: vscode.Webview, disposables?: vscode.Disposable[]) {
         this.setEventEmitter(webview);
         this.setEventReceiver(webview, disposables);
     }
 
+    /**
+     * Listener for event notification message coming from the webview.
+     */
     private setEventReceiver(webview: vscode.Webview, disposables?: vscode.Disposable[]): void {
         webview.onDidReceiveMessage(
             (message: IdeEvent) => {
@@ -29,14 +35,23 @@ export class WebviewEventManager {
         );
     }
 
+    /**
+     * Provide logic for the webview regarding how to send messages back to JFrog.
+     */
     public async setEventEmitter(webview: vscode.Webview): Promise<void> {
         await this.sendEvent(webview, { type: webviewEventType.SetEmitter, emitterFunc: 'return acquireVsCodeApi().postMessage' } as SetEmitterEvent);
     }
 
+    /**
+     * Send the page data to the webview to be display.
+     */
     public async loadPage(webview: vscode.Webview, page: WebviewPage): Promise<void> {
         await this.sendEvent(webview, { type: webviewEventType.ShowPage, pageData: page } as ShowPageEvent);
     }
 
+    /**
+     * Communication channel between JFrog extension and webview for sending data.
+     */
     private async sendEvent(webview: vscode.Webview, event: WebviewEvent): Promise<void> {
         await webview.postMessage(event);
     }
