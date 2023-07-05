5f0332c33a9729f88da8394d7685fc0e1ede4dea
Or Geva <orgab@jfrog.com>
2023-07-05 11:06:57 +0300

    Remove unused code

--- src/main/treeDataProviders/dependenciesTree/dependenciesRoot/npmTree.ts
+++ src/main/treeDataProviders/dependenciesTree/dependenciesRoot/npmTree.ts
@@ -1,4 +1,3 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
 import { DependenciesTreeNode } from '../dependenciesTreeNode';
 import { GeneralInfo } from '../../../types/generalInfo';
