b1d2c7ba9e0e6c5af1bfa9e7fd28ad1b70bc9de0
attiasas <assafattias93@gmail.com>
2023-07-19 17:40:48 +0300

    remove duplications in advance scans

--- src/main/treeDataProviders/issuesTree/codeFileTree/codeFileTreeNode.ts
+++ src/main/treeDataProviders/issuesTree/codeFileTree/codeFileTreeNode.ts
@@ -1,3 +1,4 @@
+import { AnalyzerUtils } from '../../utils/analyzerUtils';
 import { FileTreeNode } from '../fileTreeNode';
 import { IssuesRootTreeNode } from '../issuesRootTreeNode';
 import { CodeIssueTreeNode } from './codeIssueTreeNode';
@@ -11,6 +12,21 @@ export class CodeFileTreeNode extends FileTreeNode {
 
     public get issues(): CodeIssueTreeNode[] {
         return this._codeIssues;
+    }
+
+    public addIssue(node: CodeIssueTreeNode): boolean {
+        if (!this.hasSimilarIssue(node)) {
+            this._codeIssues.push(node);
+            return true;
+        }
+        return false;
+    }
+
+    private hasSimilarIssue(node: CodeIssueTreeNode) {
+        return (
+            this._codeIssues.find(issue => issue.label === node.label && AnalyzerUtils.isEqualRange(node.regionWithIssue, issue.regionWithIssue)) !=
+            undefined
+        );
     }
 
     /** @override */
