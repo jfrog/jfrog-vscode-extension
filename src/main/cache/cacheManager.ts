f14ce3e59ad0d90a0326b5e3a81ec2bb0c453431
Or Geva <orgab@jfrog.com>
2023-08-14 10:04:57 +0300

    Refactor cache class

--- src/main/cache/cacheManager.ts
+++ src/main/cache/cacheManager.ts
@@ -9,8 +9,6 @@ import { CacheRecord } from './cacheRecord';
  * Manages caching of workspace issues data in the extension.
  */
 export class CacheManager {
-    private static readonly CACHE_VERSION: number = 1;
-    private static readonly MAX_SCAN_CACHE_AGE_MILLISECS: number = 1000 * 60 * 60 * 24 * 7;
     public static readonly CACHE_BASE_KEY: string = 'jfrog.cache.issues.';
     private _cache: vscode.Memento;
 
@@ -25,8 +23,8 @@ export class CacheManager {
      * @param value - the data we want to store
      */
     public save(workspace: vscode.WorkspaceFolder, value: ScanResults) {
-        const record: CacheRecord = new CacheRecord(value, Date.now(), CacheManager.CACHE_VERSION);
-        return this._cache.update(this.toKey(workspace), JSON.stringify(record));
+        const record: CacheRecord = new CacheRecord(value, Date.now(), CacheRecord.CURRENT_CACHE_VERSION);
+        return this._cache.update(this.toCacheKey(workspace), JSON.stringify(record));
     }
 
     /**
@@ -34,38 +32,31 @@ export class CacheManager {
      * @param workspace - the workspace we want to get it's id
      * @returns - the unique key for this workspace
      */
-    private toKey(workspace: vscode.WorkspaceFolder): string {
+    private toCacheKey(workspace: vscode.WorkspaceFolder): string {
         return CacheManager.CACHE_BASE_KEY + workspace.uri.fsPath;
     }
 
     /**
-     * Get a scan results stored in the cache base on a given workspace and make sure the results are not expired.
-     * @param workspace - the workspace to search it's data
-     * @returns ScanResults if there are stored and not expired, undefined otherwise.
+     * Retrieve stored scan results from the cache for a given workspace, ensuring they not expired.
+     * @param workspace - the workspace for which to retrieve the data.
+     * @returns ScanResults if they are stored, not expired, or have the latest cache version
      */
     public async load(workspace: vscode.WorkspaceFolder): Promise<ScanResults | undefined> {
-        let rawJson: string | undefined = this._cache.get(this.toKey(workspace));
-        if (rawJson === undefined) {
-            // No cache found
-            return undefined;
-        }
+        let rawJson: string | undefined = this._cache.get(this.toCacheKey(workspace));
+        return this.toScanResult(rawJson);
+    }
+
+    private toScanResult(rawJson: string | undefined): ScanResults | undefined {
         const record: CacheRecord = CacheRecord.fromJson(rawJson);
-        if (this.invalidTimestamp(record.timestamp) || this.invalidCacheVersion(record.version) || !record.data) {
+        if (!record.isValid()) {
             vscode.window.showInformationMessage('JFrog: Scan results have expired.', ...['Rescan']).then(answer => {
                 if (answer === 'Rescan') {
                     vscode.commands.executeCommand('jfrog.scan.refresh');
                 }
             });
-            return undefined;
+            return;
         }
+
         return ScanResults.fromJson(record.data);
-    }
-
-    private invalidTimestamp(timestamp?: number): boolean {
-        return !timestamp || Date.now() - timestamp > CacheManager.MAX_SCAN_CACHE_AGE_MILLISECS;
-    }
-
-    private invalidCacheVersion(version?: number): boolean {
-        return version !== CacheManager.CACHE_VERSION;
     }
 }
--- src/main/cache/cacheRecord.ts
+++ src/main/cache/cacheRecord.ts
@@ -4,9 +4,15 @@ import { ScanResults } from '../types/workspaceIssuesDetails';
  * Represents a cached record of workspace issues data.
  */
 export class CacheRecord {
+    private static readonly MAX_CACHE_AGE_MILLISECS: number = 1000 * 60 * 60 * 24 * 7;
+    public static readonly CURRENT_CACHE_VERSION: number = 1;
+
     constructor(private _data?: ScanResults, private _timestamp?: number, private _version?: number) {}
 
-    public static fromJson(rawJson: string): CacheRecord {
+    public static fromJson(rawJson: string | undefined): CacheRecord {
+        if (rawJson === undefined) {
+            return new CacheRecord();
+        }
         const json: any = JSON.parse(rawJson);
         return new CacheRecord(json._data, json._timestamp, json._version);
     }
@@ -21,5 +27,21 @@ export class CacheRecord {
 
     public get data(): any | undefined {
         return this._data;
+    }
+
+    public isValid(): boolean {
+        return this.hasValidData() && this.isNotExpired() && this.hasCurrentVersion();
+    }
+
+    private hasValidData(): boolean {
+        return this._data !== undefined;
+    }
+
+    private isNotExpired(): boolean {
+        return this._timestamp !== undefined && Date.now() - this._timestamp <= CacheRecord.MAX_CACHE_AGE_MILLISECS;
+    }
+
+    private hasCurrentVersion(): boolean {
+        return this._version !== undefined && this._version === CacheRecord.CURRENT_CACHE_VERSION;
     }
 }
